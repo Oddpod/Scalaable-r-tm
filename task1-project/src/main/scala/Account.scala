a03b5c2f879640b87d5977f539c85e11f0eaf0c2
Andreas <andreas.borhaug@outlook.com>
2017-10-24 16:18:44 +0200

    Task 1 done

--- task1-project/src/main/scala/Account.scala
+++ task1-project/src/main/scala/Account.scala
@@ -1,5 +1,23 @@
+import exceptions.{IllegalAmountException, NoSufficientFundsException}
+
 class Account(initialBalance: Double, val uid: Int = Bank getUniqueId) {
-  def withdraw(amount: Double): Unit = ??? // Implement
-  def deposit(amount: Double): Unit = ??? // Implement
-  def getBalanceAmount: Double = ??? // Implement
+  var balance : Double = initialBalance;
+  def withdraw(amount: Double): Unit = this.synchronized {
+    if(amount > getBalanceAmount) {
+      throw new NoSufficientFundsException()
+    }  else if (amount < 0) {
+      throw new IllegalAmountException("Only positive numbers")
+    } else {
+     balance -= amount;
+    }
+  }
+  def deposit(amount: Double): Unit = this.synchronized {
+    if(amount < 0) {
+      throw new IllegalAmountException("It's a deposit, not a withdrawal");
+    }
+    balance += amount;
+  }
+  def getBalanceAmount: Double = {
+    balance;
+  }
 }
--- task1-project/src/main/scala/Bank.scala
+++ task1-project/src/main/scala/Bank.scala
@@ -2,7 +2,10 @@ object Bank {
 
   private var idCounter: Int = 0
 
-  def transaction(from: Account, to: Account, amount: Double): Unit = ??? // Implement
+  def transaction(from: Account, to: Account, amount: Double): Unit = {
+    from.withdraw(amount);
+    to.deposit(amount);
+  }
 
   def getUniqueId: Int = {
     idCounter += 1 // Can this be improved?
