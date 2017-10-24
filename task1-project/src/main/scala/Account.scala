d2f8d85ab3e7f3701cf9cbd95638da08aefa3b38
Odd Harald Sjursen Sande <oddh123@gmail.com>
2017-10-24 16:50:10 +0200

    Remove unecessary semicolons

--- task1-project/src/main/scala/Account.scala
+++ task1-project/src/main/scala/Account.scala
@@ -1,23 +1,23 @@
 import exceptions.{IllegalAmountException, NoSufficientFundsException}
 
 class Account(initialBalance: Double, val uid: Int = Bank getUniqueId) {
-  var balance : Double = initialBalance;
+  var balance : Double = initialBalance
   def withdraw(amount: Double): Unit = this.synchronized {
     if(amount > getBalanceAmount) {
       throw new NoSufficientFundsException()
     }  else if (amount < 0) {
       throw new IllegalAmountException("Only positive numbers")
     } else {
-     balance -= amount;
+     balance -= amount
     }
   }
   def deposit(amount: Double): Unit = this.synchronized {
     if(amount < 0) {
       throw new IllegalAmountException("It's a deposit, not a withdrawal");
     }
-    balance += amount;
+    balance += amount
   }
   def getBalanceAmount: Double = {
-    balance;
+    balance
   }
 }
--- task1-project/src/main/scala/Bank.scala
+++ task1-project/src/main/scala/Bank.scala
@@ -3,8 +3,8 @@ object Bank {
   private var idCounter: Int = 0
 
   def transaction(from: Account, to: Account, amount: Double): Unit = {
-    from.withdraw(amount);
-    to.deposit(amount);
+    from.withdraw(amount)
+    to.deposit(amount)
   }
 
   def getUniqueId: Int = {
